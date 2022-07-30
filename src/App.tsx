299290387934c5b84b119fa4466ab30136a6d3ae
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2022-07-31 00:02:43 +0300

    Add authProvider template

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Admin, Resource } from "react-admin";
 import UserList from "./users/list";
 import jsonServerProvider from "ra-data-json-server";
+import { authProvider } from "./providers";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 function App() {
   return (
-    <Admin dataProvider={dataProvider}>
+    <Admin dataProvider={dataProvider} authProvider={authProvider}>
       <Resource name="users" list={UserList} />
     </Admin>
   );
