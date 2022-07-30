56bb8ebcd24b2dbf9369233c108942bd1b331575
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2022-07-31 00:02:20 +0300

    Add example data list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Admin } from "react-admin";
+import { Admin, Resource } from "react-admin";
+import UserList from "./users/list";
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 function App() {
-  return <Admin dataProvider={dataProvider} />;
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={UserList} />
+    </Admin>
+  );
 }
 
 export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+import UrlField from "./UrlField";
+
+export { UrlField };
