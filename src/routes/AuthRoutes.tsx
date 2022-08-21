d976c51c9c4fdfae4a024587212b9a4122d353cb
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2022-08-21 07:24:06 +0300

    Move the login file to features folder

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -3,7 +3,7 @@ import { lazy } from "react";
 import Loadable from "components/Loadable";
 import MinimalLayout from "layout/MinimalLayout";
 
-const AuthLogin = Loadable(lazy(() => import("pages/auth/Login/Login")));
+const AuthLogin = Loadable(lazy(() => import("features/auth/Login/Login")));
 
 const LoginRoutes = {
   path: "/",
