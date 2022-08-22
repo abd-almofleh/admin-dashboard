b958055c5e6c467bfac66cedd83908b19c50594d
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2022-08-22 04:35:08 +0300

    Change the name of the login page to index

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -3,7 +3,7 @@ import { lazy } from "react";
 import Loadable from "components/Loadable";
 import RequireAuth from "features/auth/RequireAuth";
 
-const AuthLogin = Loadable(lazy(() => import("features/auth/Login/Login")));
+const AuthLogin = Loadable(lazy(() => import("features/auth/Login")));
 const LoginRoutes = {
   path: "/",
   element: <RequireAuth guestOnly />,
